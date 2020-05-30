17c7c9da955962330b2da30671660dc5adcee64c
IBXiaomi <13572226807@163.com>
2020-05-31 00:32:35 +0800

    买卖股票2

--- src/main/java/com/baxixiaomi/study/leetcode/StockTrading/StockTrading.java
+++ src/main/java/com/baxixiaomi/study/leetcode/StockTrading/StockTrading.java
@@ -2,11 +2,10 @@ package com.baxixiaomi.study.leetcode.StockTrading;
 
 /**
  * 给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
- *
+ * <p>
  * 设计一个算法来计算你所能获取的最大利润。你可以尽可能地完成更多的交易（多次买卖一支股票）。
- *
+ * <p>
  * 注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
- *
  *
  * @author 吧嘻小米
  * @date 2020/05/30
@@ -35,5 +34,47 @@ public class StockTrading {
 
     1 <= prices.length <= 3 * 10 ^ 4
     0 <= prices[i] <= 10 ^ 4
+
+   解题思路：可以当天买，当天卖，
     */
+    public static void main(String[] args) {
+        //int[] prices = new int[]{1, 2, 3, 4, 5};
+        int[] prices = new int[]{7, 1, 5, 3, 6, 4};
+        //System.out.println(maxProfit(prices));
+        calculate(prices, 0);
+    }
+
+    private static int maxProfit(int[] prices) {
+        int max = 0;
+        int min = prices[0];
+        for (int i = 0; i < prices.length; ++i) {
+            if (min < prices[i]) {
+                max += (prices[i] - min);
+            }
+            min = prices[i];
+        }
+        return max;
+    }
+
+    private static int calculate(int prices[], int s) {
+        if (s >= prices.length)
+            return 0;
+        int max = 0;
+        for (int start = s; start < prices.length; start++) {
+            int maxprofit = 0;
+            for (int i = start + 1; i < prices.length; i++) {
+                if (prices[start] < prices[i]) {
+                    int profit = calculate(prices, i + 1) + prices[i] - prices[start];
+                    if (profit > maxprofit)
+                        maxprofit = profit;
+                }
+            }
+            if (maxprofit > max)
+                max = maxprofit;
+        }
+        return max;
+    }
+
+
+
 }
